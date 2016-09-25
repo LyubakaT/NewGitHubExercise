b862819c921abf043f44197e564f9f60e94516c8
LyubakaT <mrltonchev@gmail.com>
2016-09-26 00:20:25 +0300

    Adding Complex Condition folder

---
+++ Complex-Conditions/Personal-Titles/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personal_Titles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var age = double.Parse(Console.ReadLine());
+            var gender = Console.ReadLine();
+            
+            if (gender == "m")
+            {
+                if (age < 16) Console.WriteLine("Master");
+                else Console.WriteLine("Mr.");   
+            }    
+            else
+            {
+                // We have a female
+                if (age < 16) Console.WriteLine("Miss");
+                else Console.WriteLine("Ms.");
+            }
+        }
+    }
+}
